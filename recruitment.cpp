d2fc25f9c51157f7090cbd3ac66357a40893147b
Rexmaha <37144160+Rexmaha@users.noreply.github.com>
2023-05-25 01:37:30 +0900

    FILE IO를 위한 클래스 수정
    FILE IO를 위한 클래스 수정

--- recruitment.cpp
+++ recruitment.cpp
@@ -1,13 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
 #include "recruitment.h"
+using namespace std;
 
 std::vector<Recruitment*> RecruitmentList::recruitments = std::vector<Recruitment*>(0);
 
-void RecruitmentList::addRecruitment(Recruitment* recruitment){
+void RecruitmentList::addRecruitment(Recruitment* recruitment) {
     recruitments.push_back(recruitment);
 }
 
-void AddRecruitInfo::addNewRecruit(char* work, int num, char* deadline){
-    Recruitment* r;
+void AddRecruitInfo::addNewRecruit(char* work, int num, char* deadline) {
+    Recruitment* r = new Recruitment();
     r->setWork(work);
     r->setNumberOfRecruit(num);
     r->setDeadline(deadline);
@@ -15,20 +18,23 @@ void AddRecruitInfo::addNewRecruit(char* work, int num, char* deadline){
     RecruitmentList::addRecruitment(r);
 }
 
-void AddRecruitInfoUI::createNewRecruit(char* work, int num, char* deadline){
+void AddRecruitInfoUI::createNewRecruit(char* work, int num, char* deadline) {
     AddRecruitInfo::addNewRecruit(work, num, deadline);
 }
 
-void RecruitInquiryUI::selectRecruitInfo(){
+void RecruitInquiryUI::selectRecruitInfo() {
     RecruitInquiry::showRecruitInfo();
 }
 
-void RecruitInquiry::showRecruitInfo(){
+void RecruitInquiry::showRecruitInfo() {
     RecruitmentList::getRecruitListinfo();
 }
 
-void RecruitmentList::getRecruitListinfo(){
-    for(auto recruit:recruitments){
-        cout<<recruit->getWork()<<" "<<recruit->getNumberOfRecruit()<<" "<<recruit->getDeadline()<<'\n';
+void RecruitmentList::getRecruitListinfo() {
+    FILE* out_fp;
+    out_fp = fopen("output.txt", "w+");
+    for (auto recruit : recruitments) {
+        fprintf(out_fp, "%s %d %s\n", recruit->getWork(), recruit->getNumberOfRecruit(), recruit->getDeadline());
+        cout << recruit->getWork() << " " << recruit->getNumberOfRecruit() << " " << recruit->getDeadline() << '\n';
     }
 }
--- recruitment.h
+++ recruitment.h
@@ -1,24 +1,25 @@
+#pragma once
 #include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-class Recruitment{
+class Recruitment {
 private:
     string work;
     int numberOfRecruit;
     string deadline;
 public:
-    string getWork(){ return this->work; }
-    int getNumberOfRecruit(){ return this->numberOfRecruit; }
-    string getDeadline(){ return this->deadline; }
-    void setWork(char* work){this->work = work;}
-    void setNumberOfRecruit(int num){this->numberOfRecruit = num;}
-    void setDeadline(char* deadline){this->deadline = deadline;}
+    string getWork() { return this->work; }
+    int getNumberOfRecruit() { return this->numberOfRecruit; }
+    string getDeadline() { return this->deadline; }
+    void setWork(char* work) { this->work = work; }
+    void setNumberOfRecruit(int num) { this->numberOfRecruit = num; }
+    void setDeadline(char* deadline) { this->deadline = deadline; }
 };
 
-class RecruitmentList{
+class RecruitmentList {
 private:
     static vector<Recruitment*> recruitments;
 public:
@@ -26,24 +27,24 @@ public:
     static void getRecruitListinfo();
 };
 
-class AddRecruitInfoUI{
+class AddRecruitInfoUI {
 public:
     static void createNewRecruit(char*, int, char*);
     static void startInterface();
 };
 
-class AddRecruitInfo{
+class AddRecruitInfo {
 public:
     static void addNewRecruit(char*, int, char*);
 };
 
-class RecruitInquiryUI{
+class RecruitInquiryUI {
 public:
     static void selectRecruitInfo();
     static void startInterface();
 };
 
-class RecruitInquiry{
+class RecruitInquiry {
 public:
     static void showRecruitInfo();
 };
